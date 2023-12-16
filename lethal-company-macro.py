1fb7bb934198bda7d6d1367770d36458cc83bf13
Clyde Mesa <95511845+AquilaPebble@users.noreply.github.com>
2023-12-17 08:29:04 +1300

    fixed bugs

--- lethal-company-macro.py
+++ lethal-company-macro.py
@@ -1,10 +1,12 @@
 import keyboard
 from time import sleep
-macroOn = True
+macroOn = False
 while True:
-    if keyboard.is_pressed(""):
+    if keyboard.is_pressed("`"):
         macroOn = not macroOn
     if macroOn:
-        [keyboard.press_and_release(i + " ") for i in ('b3','c1','c2','c7','d6','f2','h5','i1','j6','k9','l0','m6','m9','o5','p1','r2','r4','t2','u2','u9','v0','x8','y9','z3')]
+        for i in ('b3','c1','c2','c7','d6','f2','h5','i1','j6','k9','l0','m6','m9','o5','p1','r2','r4','t2','u2','u9','v0','x8','y9','z3'):
+            [keyboard.press_and_release(o) for o in i]
+            keyboard.press_and_release(" ")
         keyboard.press_and_release("enter")
-        sleep(0.1)
+        sleep(1)
